d097df65e280524ab955f799292682a3bf244216
Magnus Krona <magnus.krona@gmail.com>
2021-02-23 13:14:21 +0100

    Use cookies instead of bearer toeken due to signalr isssues

--- src/connection-provider.js
+++ src/connection-provider.js
@@ -1,14 +1,14 @@
 import {ConnectionContext} from './connection-context';
 import { hubConnection } from 'signalr-no-jquery';
 import {useState, useEffect} from 'react';
-import { API_URL, API_TOKEN } from './constants';
+import { API_URL } from './constants';
 const ConnectionProvider = (props) => {
 
     const [proxy, setProxy] = useState(null);
 
     useEffect(() => {
         
-        const connection = hubConnection(API_URL, { qs: { Bearer: API_TOKEN } });
+        const connection = hubConnection(API_URL);
         const hubProxy = connection.createHubProxy('rtm');
         setProxy(hubProxy);
         hubProxy.on('init', (type, data) => {}); // dummy event to get signalR started...
